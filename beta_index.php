1de56eeaa4b74a399ceb8821f943a0539d381b77
Tony.Hansen <tony.hansen@pioneer.com>
2017-07-14 14:09:46 -0500

    Basic layout for homepage

---
+++ beta_index.php
@@ -0,0 +1,97 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN"
+	"http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
+<!--
+	Design by TEMPLATED
+	http://templated.co
+	Released for free under the Creative Commons Attribution License
+	Name       : Nameless Geometry
+	Version    : 1.0
+	Released   : 20130222
+-->
+<html xmlns="http://www.w3.org/1999/xhtml">
+	<head> 
+		<meta name="keywords" content="" />
+		<meta name="description" content="" />
+		<meta http-equiv="content-type" content="text/html; charset=utf-8" />
+		<title>Des Moines Adult Hockey - Home</title>
+		<link href="http://fonts.googleapis.com/css?family=Yanone+Kaffeesatz" rel="stylesheet" type="text/css" />
+		<link rel="stylesheet" type="text/css" href="common/style.css" />
+	</head>
+	<body>
+	<?php include ("common/tracking.php");?>
+		<div id="bg">
+			<div id="outer">
+				<div id="header">
+				<?php include ("common/header.php");?>
+				</div>
+				<div id="main">
+					<div id="sidebar1">
+						<?php include ("common/teams.php");?>
+					</div>
+					<div id="sidebar2">
+							<?php include ("common/recent_scores.php");?>
+					</div>
+					<div id="content">
+						<div id="box1">
+							<h2>
+								Welcome to Des Moines Adult Hockey
+							</h2>
+							<img class="left" src="images/pic1.jpg" width="150" height="190" alt="" />
+							<p>
+								We are getting ready for the 2017-18 hockey season.  This will be the place to come for the league schedule, standings, stats, etc.  Keep checking back for more information as we get closer to the start of the season.
+							</p>
+						</div>
+						<div id="box2">
+							<h3>
+								League News
+							</h3>
+							<ul class="imageList">
+								<li class="first">
+									<!--<img class="left" src="images/pic2.jpg" width="64" height="64" alt="" />-->
+									<p>The website is undergoing a redesign currently, but I'll have the schedule posted once approved by the rink.</p>
+								</li>
+								<!--
+								<li>
+									<img class="left" src="images/pic2.jpg" width="64" height="64" alt="" />
+									<p>Team pages show updated rosters and stats for each player on the team.</p>
+								</li>
+								<li class="last">
+									<img class="left" src="images/pic2.jpg" width="64" height="64" alt="" />
+									<p>Check back frequently as more functionality will be added to the site in the coming weeks.</p>
+								</li>-->
+							</ul>
+						</div>
+						<div id="box3">
+							<h3>
+								Important Links
+							</h3>
+							<p>
+								Below are some helpful and frequently needed links for the league:
+							</p>
+							<ul class="linkedList">
+								<li class="first">
+									<a href="http://www.jbchockey.com/register.html" target="_blank">2017-18 League Registration</a>
+								</li>
+								<!--<li>
+									<a href="https://sites.google.com/site/abchockeydsm/registraion" target="_blank">March 21st 3-on-3 Tournament</a>
+								</li>
+								<li class="last">
+									<a href="other/discipline-flowchart.pdf" target="_blank">Revised Discipline Flowchart</a>
+								</li>-->
+							
+							</ul>
+						</div>
+						<br class="clear" />
+					</div>
+					<br class="clear" />
+				</div>
+				<div id="footer">
+					<?php include ("common/footer.php");?>
+				</div>
+			</div>
+			<div id="copyright">
+				&copy; DMAHockey.com | Design by <a href="http://templated.co" rel="nofollow">TEMPLATED</a>
+			</div>
+		</div>
+	</body>
+</html>
---
+++ common/header.php
@@ -0,0 +1,29 @@
+<?php
+echo '<div id="logo">
+						<h1>
+							<a href="index.php">
+							<img class="left" src="images/ABC_Logo.png" width="127" height="120" alt="ABC_Logo" />
+							Des Moines Adult Hockey</a>
+						</h1>
+					</div>
+					<div id="nav">
+						<ul>
+							<li class="first active">
+								<a href="index.php">Home</a>
+							</li>
+							<li>
+								<a href="index.php">Schedule</a>
+							</li>
+							<li>
+								<a href="index.php">Standings</a>
+							</li>
+							<li>
+								<a href="index.php">Stats</a>
+							</li>
+							<li class="last">
+								<a href="index">Contact</a>
+							</li>
+						</ul>
+						<br class="clear" />
+					</div>';
+?>
